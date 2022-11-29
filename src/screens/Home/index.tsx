91612db527ec0b99ffc934220bc1958ff6861a01
AlexandreLira <alexandrelira112@gmail.com>
2022-11-29 18:12:34 -0300

    Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo from '../../assets/logo.svg';
+
 import {
-    Container, Title
+    Container,
+    Header,
+    TotalCars
 } from './styles';
 
-export function Home(){
+export function Home() {
     return (
         <Container>
-            <Title>Home</Title>
+            <StatusBar style="light" />
+            <Header>
+                <Logo
+                    width={RFValue(108)}
+                    height={RFValue(12)}
+                />
+                <TotalCars>Total de 11 carros</TotalCars>
+            </Header>
         </Container>
     )
 }
