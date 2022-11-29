543c2d4c974e5663f1abdc6af66d9d7de008fcd1
AlexandreLira <alexandrelira112@gmail.com>
2022-11-29 17:19:22 -0300

    global theme

---
+++ App.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback } from 'react';
+import { Home } from './src/screens/Home';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+
+import {
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView} style={{flex: 1}}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </View>
+  );
+}
